7b9751eb37a70a2aa5e10911f9b58ab5c038ac2d
EllaBsh <61976580+EllaBsh@users.noreply.github.com>
2026-01-02 14:19:23 +0200

    add upload file function

--- client/src/Components/Buttons/FileActions/UploadFile.jsx
+++ client/src/Components/Buttons/FileActions/UploadFile.jsx
@@ -1,13 +1,46 @@
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import { Button } from '@mui/material';
+import mammoth from 'mammoth';
+import { useRef } from 'react';
 import sxStyles from './sxStyles';
 
-const UploadFile = () => {
+const UploadFile = ({ setUnseenText }) => {
+    const fileInputRef = useRef();
+    const handleChange = async (event) => {
+        const file = event.target.files[0];
+        const fileType = file.name.split('.').pop().toLowerCase();
+        try {
+            if (fileType === 'txt') {
+                setUnseenText(file.text());
+            } else if (fileType === 'docx') {
+                const arrayBuffer = await file.arrayBuffer();
+                const result = await mammoth.extractRawText({ arrayBuffer });
+                setUnseenText(result.value);
+            }
+        } catch (err) {
+            setUnseenText('Failed to read file. Is it a valid DOCX?');
+        }
+    };
+
     return (
-        <Button sx={sxStyles.button} disableRipple disableFocusRipple>
-            <FileUploadOutlinedIcon sx={sxStyles.icon} />
-            Upload Text File
-        </Button>
+        <>
+            <Button
+                sx={sxStyles.button}
+                onClick={() => fileInputRef.current.click()}
+                disableRipple
+                disableFocusRipple>
+                <FileUploadOutlinedIcon sx={sxStyles.icon} />
+                Upload Text File
+            </Button>
+            <input
+                onChange={handleChange}
+                multiple={false}
+                ref={fileInputRef}
+                type='file'
+                accept='.txt, .docx'
+                hidden
+            />
+        </>
     );
 };
 
